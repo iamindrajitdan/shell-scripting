b65628040433d5f4bd26b256c03c7004dc501681
iamindrajitdan <iamindrajitdan@gmail.com>
2026-02-01 10:34:34 +0000

    Auto-commit: Sun Feb  1 10:34:34 UTC 2026

--- deploying-app.sh
+++ deploying-app.sh
@@ -7,13 +7,13 @@ git_clone(){
 }
 
 install_requirements(){
-        sudo apt update && sudo apt install -y  docker.io docker-compose nginx
+        sudo apt update && sudo apt upgrade -y && sudo apt install -y  docker.io docker-compose nginx
 }
 
 
-restart_services(){
+perm_serv(){
         sudo chown "$USER"  /var/run/docker.sock
-        sudo chmod 777 /home/ubuntu/shell-scripting/django-notes-app/data/mysql/db/#ib_16384_1.dblwr
+	#sudo chown "$USER" /home/ubuntu/shell-scripting/django-notes-app/data/mysql/db
         #sudo systemctl enable docker
         #sudo systemctl enable nginx
 }
@@ -21,7 +21,7 @@ restart_services(){
 deploy(){
         docker build -t notes-app .
         docker-compose up -d
-        docker run -d -p 8000:8000 notes-app:latest
+        #docker run -d -p 8000:8000 notes-app:latest
 }
 
 
@@ -30,14 +30,19 @@ echo "DEPLOYMENT STARTED"
 echo "####################################################################"
 
 if ! git_clone; then
+	echo "####################################################################"
         echo "Already exists"
+	echo "####################################################################"
         cd django-notes-app
 fi
 install_requirements
-restart_services
+perm_serv
 if ! deploy; then
-        sudo systemctl stop nginx && sudo fuser -k 80/tcp 8000/tcp && docker compose up -d
-        deploy
+        #sudo systemctl stop nginx && sudo fuser -k 80/tcp 8000/tcp && docker compose up -d
+        echo "####################################################################"
+	echo "DEPLOYMENT STARTED"
+	echo "####################################################################"
+	exit 1
 fi
 
                                       
