16dd25264b4c82a791a5f278de39f2f407d721cf
iamindrajitdan <iamindrajitdan@gmail.com>
2026-02-01 08:31:19 +0000

    Auto-commit: Sun Feb  1 08:31:19 UTC 2026

--- deploying-app.sh
+++ deploying-app.sh
@@ -2,24 +2,42 @@
 #This is an deployment script of an django app
 
 git_clone(){
-	git clone https://github.com/LondheShubham153/django-notes-app.git
-	cd django-notes-app
+        git clone https://github.com/LondheShubham153/django-notes-app.git
+        cd django-notes-app
 }
 
 install_requirements(){
-	sudo apt-get install docker.io python nginx -y
+        sudo apt update && sudo apt install -y  docker.io docker-compose nginx
 }
+
 
 restart_services(){
-	sudo systemctl restart docker
-	sudo systemctl restart nginx
-}
-
-build_docker_image(){
-	docker build -t notes-app .
+        sudo chown "$USER"  /var/run/docker.sock
+        sudo chmod 777 /home/ubuntu/shell-scripting/django-notes-app/data/mysql/db/#ib_16384_1.dblwr
+        #sudo systemctl enable docker
+        #sudo systemctl enable nginx
 }
 
 deploy(){
-	docker run -d -p 8000:8000 notes-app:latest
+        docker build -t notes-app .
+        docker-compose up -d
+        docker run -d -p 8000:8000 notes-app:latest
 }
 
+
+echo "####################################################################"
+echo "DEPLOYMENT STARTED"
+echo "####################################################################"
+
+if ! git_clone; then
+        echo "Already exists"
+        cd django-notes-app
+fi
+install_requirements
+restart_services
+if ! deploy; then
+        sudo systemctl stop nginx && sudo fuser -k 80/tcp 8000/tcp && docker compose up -d
+        deploy
+fi
+
+                                      
